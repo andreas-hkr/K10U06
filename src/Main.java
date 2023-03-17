36863c98594c9cd2750d2c888b281f2eae991596
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-17 17:28:52 +0100

    Solved (+updated gitignore)

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,35 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    static String translate(String text) {
+        String result = "";
+        for (int i= 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            result += switch (c) {
+                case 'a', 'e', 'i', 'o', 'u', 'y', 'å', 'ä', 'ö' -> c;
+                case ' ', '.' -> c;
+                default -> c + "o" + c;
+            };
+        }
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
         // Översätt en inläst text till rövarspråk och spara den i en textfil
 
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ange en text: ");
+        String text = input.nextLine();
 
+        String translated = translate(text);
+        System.out.println("Översatt: " + translated);
+
+        PrintWriter fileOut = new PrintWriter(new BufferedWriter(new FileWriter("translated.txt")));
+        fileOut.println(translated);
+        fileOut.close();
     }
 }
