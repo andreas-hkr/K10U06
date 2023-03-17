f2c721cc8ae385a52c5ab8102bf74dbb304089d3
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-17 17:16:05 +0100

    Fixed comment

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // Översätt en inläst text till rövarspråk och spara i en textfil
+        // Översätt en inläst text till rövarspråk och spara den i en textfil
 
 
     }
